72b31608ebde2398ab80af7cc1615c046f51cdf1
George Dilthey <george.dilthey@gmail.com>
2021-02-06 13:27:06 -0500

    commit tree 0a5389a112e57dae9e9d30e6a31372284d6830e1

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    
+    length = array.length
+    if length == 1 then
+        return array.join
+    elsif length == 2 then
+        return array.join " and "
+    else
+        last_item = array[length-1].to_s
+        array[length-1] = "and " << last_item
+        return array.join(", ");
+    end
 end
